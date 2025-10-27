c6ebf57ed81c20f291a0c1c586ae47ad7b15e161
Lucas Silva <pedrolucasdacosta.14@gmail.com>
2025-10-27 10:23:36 -0300

    finalizando as configuração do preojeto

---
+++ src/components/teste/teste.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import { Container, Title } from './testestyled';
+
+export default function Teste() {
+  return (
+    <Container>
+      <Title>
+        Olá, <span>Somos Todos Marianas 💛</span>
+      </Title>
+    </Container>
+  );
+}
