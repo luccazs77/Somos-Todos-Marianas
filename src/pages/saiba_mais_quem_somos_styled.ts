c3de8f1bfb4661713b6b7163365a3364e705f802
alvaro <alvarosimeao21@gmail.com>
2025-11-04 12:09:41 -0300

    finalizando o primeiro saiba mais

---
+++ src/pages/saiba_mais_quem_somos_styled.ts
@@ -0,0 +1,66 @@
+import styled from "styled-components";
+
+
+export const Img16 = styled.img`
+display: flex;
+justify-content: flex-start;
+`
+
+export const Img8 = styled.img`
+display: flex;
+justify-content: flex-end;
+`
+
+export const ImgFesta = styled.img`
+padding: 50px 0px;
+`
+
+export const AllContainer = styled.body`
+background-color: #FBEEEC;
+`
+
+export const TextosSobreNosInicio = styled.div`
+display: flex;
+flex-direction: column;
+`
+
+export const SobreNos= styled.h1` 
+color:#010101;
+padding: 15px 0px 30px 0px;
+text-align: justify;
+max-width: 650px;
+margin: 0 auto;
+`
+
+export const Texto = styled.article`
+color: rgb 010101;
+font-size: 16px;
+align-self: flex-start;
+padding: 0px 30px;
+text-align: left;
+font-family: 'Albert Sans', sans-serif;
+`
+
+export const Titulo = styled.h3`
+color:rgb 111;
+font-size: 22px;
+align-self: flex-start;
+padding: 0px 0px 0px 30px;
+text-align: left;
+font-family: 'Albert Sans', sans-serif;
+`
+
+export const ContainerInicial = styled.div`
+display:flex;
+flex-direction:row;
+gap: 20px;
+`
+ 
+export const ContainerFinal = styled.div`
+display: flex;
+`
+
+export const TextosSobreNosFinal = styled.div`
+display: flex;
+flex-direction: column;
+`
